304702eaf8282b1e68763669a086de64bcb37927
OvaisFareed <movaissiddiqui16@gmail.com>
2022-11-22 20:45:47 +0500

    - Done content insertion for main page - Added/Modified collections and components

--- src/api/section/controllers/section.js
+++ src/api/section/controllers/section.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * home controller
+ * section controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController('api::section.section');
